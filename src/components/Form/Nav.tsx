ef5d90df809e1af24e8a51abc54b2d0c15d42844
ccWebDev003 <162235405+ccWebDev003@users.noreply.github.com>
2024-04-07 12:35:42 +0500

    Update Nav.tsx

--- src/components/Form/Nav.tsx
+++ src/components/Form/Nav.tsx
@@ -1,31 +1,38 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-import { StyledCard } from 'components/Form/Card';
-import Heading from 'components/Form/Heading';
-import colors from 'styles/colors';
-import { ReactNode } from 'react';
+import { StyledCard } from 'components/Form/Card'
+import Heading from 'components/Form/Heading'
+import colors from 'styles/colors'
+import { ReactNode } from 'react'
 
 const Header = styled(StyledCard)`
-  margin: 1rem auto;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: baseline;
-  justify-content: space-between;
-  padding: 0.5rem 1rem;
-  align-items: center;
-  width: 95vw;
-`;
+	margin: auto;
+	display: flex;
+	flex-wrap: wrap;
+	align-items: baseline;
+	justify-content: space-between;
+	padding: 0.5rem 2rem;
+	align-items: center;
+	width: 100vw;
+	position: sticky;
+	top: 0;
+	width: 100vw;
+	z-index: 1000;
+	flex: 1 1 auto;
+`
 
-const Nav = (props: { children?: ReactNode}) => {
-  return (
-    <Header as="header">
-    <Heading color={colors.primary} size="large">
-      <img width="64" src="/web-check.png" alt="Web Check Icon" />
-      <a href="/">Web Check</a>
-    </Heading>
-      {props.children && props.children}
-  </Header>
-  );
-};
+const Nav = (props: { children?: ReactNode }) => {
+	return (
+		<Header as='header'>
+			<Heading color={colors.primary} size='medium'>
+				<img width='50' src='/web-check.png' alt='Web Check Icon' />
+				<a style={{ fontSize: '20px' }} href='/'>
+					Web Check
+				</a>
+			</Heading>
+			{props.children && props.children}
+		</Header>
+	)
+}
 
-export default Nav;
+export default Nav
