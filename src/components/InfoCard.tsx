d77ac5c68929d690958782e900deb08dd7d06186
ccWebDev003 <162235405+ccWebDev003@users.noreply.github.com>
2024-04-07 13:05:53 +0500

    Update InfoCard.tsx

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -2,11 +2,12 @@ import { FaArrowRightLong } from 'react-icons/fa6'
 import backicon from '../assets/back-icon.png';
 import checkicon from '../assets/check-icon.png';
 import webicon from '../assets/web-icon.png';
+import { Link } from 'react-router-dom'
 
 function InfoCard() {
 	return (
 		<div className='in-cards'>
-			<div className='in-card-item'>
+			<Link to="/check-url" className='in-card-item'>
 				<p>URL tekshirish</p>
 				<span className='ic-icon'>
 					<img src={checkicon} alt='icon' />
@@ -14,17 +15,17 @@ function InfoCard() {
 				<span className='ic-arrow'>
 					<FaArrowRightLong />
 				</span>
-			</div>
-			<div className='in-card-item'>
-				<p>Saytni ko'rib chiqish</p>
+			</Link>
+			<Link to="/about" className='in-card-item'>
+				<p>Sayt haqida ma'lumot</p>
 				<span className='ic-icon'>
 					<img src={webicon} alt='icon' />
 				</span>
 				<span className='ic-arrow'>
 					<FaArrowRightLong />
 				</span>
-			</div>
-			<div className='in-card-item'>
+			</Link>
+			<Link to="/check-url" className='in-card-item'>
 				<p>Sayt ma'lumotlarini olish</p>
 				<span className='ic-icon'>
 					<img src={backicon} alt='icon' />
@@ -32,7 +33,7 @@ function InfoCard() {
 				<span className='ic-arrow'>
 					<FaArrowRightLong />
 				</span>
-			</div>
+			</Link>
 		</div>
 	)
 }
